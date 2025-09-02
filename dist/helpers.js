5ad283ddbadb138e57e60ef1bc89cd0e2c801db5
CanseFr <jucansell@gmail.com>
2025-09-02 09:53:13 +0200

    :tada: TEst

---
+++ dist/helpers.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isEmpty = void 0;
+function isEmpty(value) {
+    return value === null || value === undefined || value === "";
+}
+exports.isEmpty = isEmpty;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+export * from "./types";
+export * from "./helpers";
+
+const test : Identifiable =
